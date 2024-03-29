a0537ebd2d69f8a710c1539d769ad7fdb8502160
VictoriaDudu <dyudyunova@inbox.ru>
2024-03-29 09:35:39 +0700

    счет гласных в строке

---
+++ vowels.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    const int N = 12;
+    char vowel[N] = { 'A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'Y', 'y' };
+    string str;
+    cout << "Enter the string: " << endl;
+    getline(cin, str);
+    int kolichestvo = 0;
+    for (int i = 0; str[i]; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (str[i] == vowel[j])
+                kolichestvo++;
+        }
+    }
+    if (kolichestvo)
+        cout << "number of vowels: " << kolichestvo << endl;
+    else
+        cout << "no vowels" << endl;
+    return 0;
+}
